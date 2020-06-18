186741f5f9c4cf60f83f8ac295f4c87222712cc4
losrobbos <losrobbos@web.de>
2020-06-18 12:12:16 +0200

    Created context for pizza data and shared it

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PizzasProvider from './contexts/PizzasContext';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <PizzasProvider>
     <App />
-  </React.StrictMode>,
+  </PizzasProvider>,
   document.getElementById('root')
 );
 
